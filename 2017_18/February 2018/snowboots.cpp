a746abf81423753c2aeeb22ebe9b1f550f137105
Pearl Yu <77512088+codexistent@users.noreply.github.com>
2023-02-07 11:15:38 -0600

    Create snowboots.cpp

---
+++ 2017_18/February 2018/snowboots.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <algorithm>
+#include <set>
+using namespace std;
+#define MAXN 100005
+
+class Boot {
+    public:
+        int id, d, s;
+};
+class Tile {
+    public:
+        int d, l, r;
+};
+
+int N, M, S = 1;
+bool V[MAXN];
+Boot B[MAXN];
+Tile T[MAXN];
+int b[MAXN], t[MAXN];
+bool cmpT(int a, int b){
+    return T[a].d > T[b].d;
+}
+bool cmpB(int a, int b) {
+    return B[a].d > B[b].d;
+};
+
+int main(){
+    cin >> N >> M;
+    for(int i = 1; i <= N; i++) { // get tiles
+        t[i] = i;
+        cin >> T[i].d;
+        T[i].l = i - 1;
+        T[i].r = i + 1;
+    }
+    for(int i = 1; i <= M; i++) { // get boots
+        B[i].id = b[i] = i;
+        cin >> B[i].d >> B[i].s;
+    }
+    T[1].l = 1; T[N].r = N;
+    sort(t + 1, t + 1 + N, cmpT);
+    sort(b + 1, b + 1 + M, cmpB);
+
+    int c = 1;
+    for(int i = 1; i <= M; i++){
+        while(c <= N && T[t[c]].d > B[b[i]].d){
+            int l = T[t[c]].l;
+            int r = T[t[c]].r;
+
+            T[l].r = r;
+            T[r].l = l;
+
+            S = max(S, r - l);
+            c++;
+        }
+
+        V[b[i]] = (S <= B[b[i]].s);
+    }
+
+    for(int i = 1; i <= M; i++) cout << V[i] << endl;
+}
