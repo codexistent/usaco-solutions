94a1330b23a8845ac6313ba5ad1f5421002b0992
Pearl Yu <77512088+codexistent@users.noreply.github.com>
2023-12-10 10:37:41 -0600

    Create sum_segtree_wlazyassign.cpp

---
+++ Algorithms/Gold/sum_segtree_wlazyassign.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i, a, b) for(ll i = a; i <= b; i++)
+#define ll long long
+
+struct SumSegmentTree{
+public:
+    vector<ll> tree, assign;
+    vector<bool> lazy;
+    ll n;
+
+    SumSegmentTree(ll n) {
+        this->n = n;
+        tree.assign(4 * this->n + 1, 0);
+        lazy.assign(4 * this->n + 1, false);
+        assign.assign(4 * this->n + 1, 0);
+    }
+    SumSegmentTree(const vector<ll> &a) : SumSegmentTree(a.size()){
+        build(a, 1, 1, n);
+    }
+    
+    void update(ll i, ll x){ // poll update
+        update_range(1, 1, n, make_tuple(i, i, x));
+    }
+    void update(ll i, ll j, ll x){
+        update_range(1, 1, n, make_tuple(i, j, x));
+    }
+    ll query(ll i){
+        return query_range(1, 1, n, make_pair(i, i));
+    }
+    ll query(ll i, ll j){
+        return query_range(1, 1, n, make_pair(i, j));
+    }
+
+private:
+    void push(int i, int l, int r){
+        if(lazy[i]){
+            int m = (l + r) / 2;
+
+            tree[i*2] = (m - l + 1) * assign[i];
+            tree[i*2 + 1] = (r - m) * assign[i];
+            lazy[i*2] = lazy[i*2 + 1] = true;
+            assign[i*2] = assign[i*2 + 1] = assign[i];
+
+            lazy[i] = false;
+        }
+    }
+
+    void build(const vector<ll> &a, ll i, ll l, ll r){
+        if(l == r) {
+            tree[i] = a[l - 1];
+            return;
+        }
+        
+        ll m = (l + r) / 2;
+        build(a, 2*i, l, m);
+        build(a, 2*i + 1, m + 1, r);
+
+        tree[i] = tree[2*i] + tree[2*i + 1];
+    }
+    void update_range(ll i, ll l, ll r, tuple<ll, ll, ll> upd){
+        cout << " " << i << " " << l << "-" << r << " ~~ " << get<0>(upd) << ", " << get<1>(upd) << ", " << get<2>(upd) << endl;
+        if(l > r) return;
+
+        if(get<0>(upd) == l && get<1>(upd) == r){
+            tree[i] = (r - l + 1) * get<2>(upd);
+            lazy[i] = true;
+            assign[i] = get<2>(upd);
+        } else {
+            push(i, l, r);
+            ll m = (l + r) / 2;
+            
+            if(get<0>(upd) < min(get<1>(upd), m)) update_range(i * 2, l, m, make_tuple(get<0>(upd), min(get<1>(upd), m), get<2>(upd)));
+            if(max(get<0>(upd), m + 1) < get<1>(upd)) update_range(i * 2 + 1, m + 1, r, make_tuple(max(get<0>(upd), m + 1), get<1>(upd), get<2>(upd)));
+
+            tree[i] = tree[i * 2] + tree[i * 2 + 1];
+        }
+    }
+    ll query_range(ll i, ll l, ll r, pair<ll, ll> qry){
+        if(l > r || qry.second < l || r < qry.first) return 0;
+
+        if(qry.first <= l && r <= qry.second) return tree[i];
+
+        push(i, l, r);
+
+        ll m = (l + r) / 2;
+        return query_range(i * 2, l, m, qry) + query_range(i * 2 + 1, m + 1, r, qry);
+    }
+};
+
+int main() {
+}
